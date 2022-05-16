1b1324131b9e670be664ed540f060ea3428118ca
InkaOro <65492226+InkaOro@users.noreply.github.com>
2022-05-16 14:03:03 +0100

    updated

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,16 +1,13 @@
 class QuestionsController < ApplicationController
-
-  def ask
-  end
+  def ask; end
 
   def answer
-
-    if params[:question] == 'I am going to work right now!'
-      @answer = 'Great!'
-    elsif params[:question].include?('?')
-      @answer = 'Silly question, get dressed and go to work!'
-    else
-      @answer = "I don't care, get dressed and go to work!"
-    end
+    @answer = if params[:question] == 'I am going to work right now!'
+                'Great!'
+              elsif params[:question].include?('?')
+                'Silly question, get dressed and go to work!'
+              else
+                "I don't care, get dressed and go to work!"
+              end
   end
 end
